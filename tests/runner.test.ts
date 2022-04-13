ff87532c9d4ccac1bff224205e75006da12c213b
dushangchen <dscupup@gmail.com>
2022-04-13 03:06:38 -0700

    fix

--- tests/runner.test.ts
+++ tests/runner.test.ts
@@ -62,14 +62,9 @@ describe('run(source, config) function', () => {
         expect(importObject.output).to.equal("2\n");
     });
 
-    it('expression', async () => {
+    it('parenthesis expression', async () => {
         await runTest("x:int = 0\nx=(2 + 3) * (5 + 10 // 4)\nprint(x)");
         expect(importObject.output).to.equal("35\n");
-    });
-
-    it('expression', async () => {
-        var result = await runTest("(2 + 3");
-        expect(result).to.equal(5);
     });
 });
 
